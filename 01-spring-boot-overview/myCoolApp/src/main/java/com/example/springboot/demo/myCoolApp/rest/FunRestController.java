e1973ac7025119ac3323686e3fd594cf8b4eb65a
Yavuz Ucarkus <81562942+yavuzucrkss@users.noreply.github.com>
2023-09-25 12:58:15 +0300

    proporties-demo

--- 01-spring-boot-overview/myCoolApp/src/main/java/com/example/springboot/demo/myCoolApp/rest/FunRestController.java
+++ 01-spring-boot-overview/myCoolApp/src/main/java/com/example/springboot/demo/myCoolApp/rest/FunRestController.java
@@ -1,10 +1,21 @@
 package com.example.springboot.demo.myCoolApp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for: coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String teamInfo() { return "Coach Name: " + coachName + " Team Name: " + teamName; }
 
     @GetMapping("/")
     public String sayHello(){
